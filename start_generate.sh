2a22bd59ea240be3c573546773130ccd66317afa
Semyon <b-semen-b@mail.ru>
2022-04-14 14:33:18 +0300

    add not requirement variable filename

--- start_generate.sh
+++ start_generate.sh
@@ -1,19 +1,32 @@
 #!/bin/bash
+generateLog () {
+    echo "admin" > "logings.txt"
+}
 if [[ $1 = "-h" ]] || [[ $1 = "--help" ]]
 then
     echo "first argument must be a file name like logging.txt"
     echo "second argument must be integer value passwords generation"
     exit
 fi
-
-if [ $1 = "[[:alnum:]].txt" ]
+re='^[0-9]+$'
+if [[ $1 =~ $re ]] && [[ -z "$2" ]]
 then
-    echo "File name can't be empty!"
-    exit
+    generateLog
+    file="logings.txt"
+else
+    file=$1
 fi
-if [ $2 -lt 1 ]
+if [[ $2 -lt 1 ]] && [[ $2 =~ $re ]]
 then
     echo "PassCount must be greate then 0"
     exit
+elif [[ $2 =~ $re ]] && [[ $2 -gt 1 ]]
+then
+    count=$2
+else
+    count=$1
 fi
-./generate_log_pass -file $1 -passCount $2
+echo " $file $count"
+./generate_log_pass -file $file -passCount $count
+
+
